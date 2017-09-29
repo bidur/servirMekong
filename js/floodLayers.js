220a9589438a1bbe782dd3d7a25d054dee718438
Hiroyuki Miyazaki <heromiya@heromiya.net>
2017-09-30 05:14:45 +0900

    Added the Simulated flood depth (1991)

---
+++ js/floodLayers.js
@@ -0,0 +1,9 @@
+var flddph1991 =
+    new ol.layer.Tile({title: 'Simulated flood depth (1991)',
+		       source: new ol.source.TileWMS ({
+			   url: 'http://guam.csis.u-tokyo.ac.jp:8080/geoserver/servir/wms?',
+			   params:{'LAYERS':'servir:flddph1991','TILED': true},
+			   serverType: 'geoserver'			
+		       })
+		       ,visible: false
+		      });
