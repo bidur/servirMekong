c345a97a0abb89e58863b84587a71c0967f0a186
bidur <bidur.devkota@gmail.com>
2017-12-04 10:53:03 +0700

    function updated with div element ID

--- js/opacitySlider.js
+++ js/opacitySlider.js
@@ -3,11 +3,14 @@ function sliderOpacity(Layer,elemID,opacity){
     Layer.setOpacity(opacity);
     Layer.setVisible(true);// Layer is made visible, if its transparency is changed by slider movement
     if(elemID != 'NA') {
-	showLegend(document.getElementById(elemID)) ;// hrsl2010 is the div ID of the legend for this layer
+		showLegend(document.getElementById(elemID)) ;// hrsl2010 is the div ID of the legend for this layer
     }
 };
-function sliderUpdate(Layer, slider,opacity) {
+function sliderUpdate(Layer, slider,opacity,elemID) {
     Layer.setOpacity(opacity);				
     slider.value = opacity *100;
     Layer.setVisible(true); // set the layer visible if its selected for setting transparency
+    if(elemID != 'NA') {
+		showLegend(document.getElementById(elemID)) ;// hrsl2010 is the div ID of the legend for this layer
+    }
 }
